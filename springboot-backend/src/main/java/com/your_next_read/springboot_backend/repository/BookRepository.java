c1070c2fd781e3b8631bcc5b121e6b85b3e1bb1c
Elga de Klerk <elga.de.klerk@gmail.com>
2026-07-18 17:03:12 +0200

    Fix problems

--- springboot-backend/src/main/java/com/your_next_read/springboot_backend/repository/BookRepository.java
+++ springboot-backend/src/main/java/com/your_next_read/springboot_backend/repository/BookRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByTags_NameIn(List<String> tagNames);
     List<Book> findByOnTbrTrue();
 }
