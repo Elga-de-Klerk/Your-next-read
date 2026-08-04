d788448613eb466e1476a8d72b634d205c7a0d25
Elga de Klerk <elga.de.klerk@gmail.com>
2026-08-04 12:34:10 +0200

    Setup base

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
 import NavBar from "./components/NavBar/NavBar.vue";
-import {ref} from "vue";
-
-const sidebarOpen = ref(true);
-
 </script>
 
 <template>
-  <NavBar @sideBarOpen="open => sidebarOpen = open"/>
+  <NavBar />
 
-  <main :class="{ expanded: sidebarOpen }" >
-    <header class="header">
-      <h1>Your Next Read</h1>
-    </header>
+  <main>
     <router-view />
   </main>
 </template>
 
 <style lang="scss" src="./App.scss" scoped></style>
-
-@removed="id => books = books.filter(b => b.id !== id)" />
--- vue-frontend/src/components/NavBar/NavBar.vue
+++ vue-frontend/src/components/NavBar/NavBar.vue
@@ -1,47 +1,34 @@
-<script setup lang="ts">
-import {ref} from "vue";
-
-const emit = defineEmits<{ sideBarOpen: [boolean] }>()
-const isSidebarOpen = ref(true);
-
-function toggleSidebar() {
-  isSidebarOpen.value = !isSidebarOpen.value
-  emit("sideBarOpen", isSidebarOpen.value);
-}
-</script>
-
 <template>
-  <aside :class="{ collapsed: isSidebarOpen }" class="sidebar">
+  <div class="sidebar">
+    <div class="title">
+      <h1>Your Next Read</h1>
+    </div>
+
     <nav class="menu">
       <ul>
-        <router-link to="/search">
-          <li class="menu-item">
+        <li class="menu-item">
+          <router-link to="/search">
             <span class="material-symbols-outlined">search</span>
             <span class="label">Search</span>
-          </li>
-        </router-link>
+          </router-link>
+        </li>
 
-        <router-link to="/tbr">
-          <li class="menu-item">
+        <li class="menu-item">
+          <router-link to="/tbr">
             <span class="material-symbols-outlined">library_books</span>
             <span class="label">My Shelf</span>
-          </li>
-        </router-link>
+          </router-link>
+        </li>
 
-        <router-link to="/next-read">
-          <li class="menu-item">
+        <li class="menu-item">
+          <router-link to="/next-read">
             <span class="material-symbols-outlined">auto_awesome</span>
             <span class="label">Pick for Me</span>
-          </li>
-        </router-link>
+          </router-link>
+        </li>
       </ul>
     </nav>
-
-    <button class="menu-item nav-menu-button" @click="toggleSidebar()">
-      <span class="material-symbols-outlined">menu</span>
-      <span class="label">Collapse</span>
-    </button>
-  </aside>
+  </div>
 </template>
 
 <style lang="scss" src="./NavBar.scss" scoped></style>
