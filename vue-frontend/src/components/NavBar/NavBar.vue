c08c7973fd6b45813dc66edf0731c31823b39344
Elga de Klerk <elga.de.klerk@gmail.com>
2026-08-04 13:51:32 +0200

    Styling mobile

--- vue-frontend/src/components/NavBar/NavBar.vue
+++ vue-frontend/src/components/NavBar/NavBar.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="sidebar">
     <div class="title">
-      <h1>Your Next Read</h1>
+      <h1>The Little Book Nook</h1>
     </div>
 
     <nav class="menu">
       <ul>
         <li class="menu-item">
           <router-link to="/search">
-            <span class="material-symbols-outlined">search</span>
+            <span class="icon material-symbols-outlined">search</span>
             <span class="label">Search</span>
           </router-link>
         </li>
 
         <li class="menu-item">
           <router-link to="/tbr">
-            <span class="material-symbols-outlined">library_books</span>
+            <span class="icon material-symbols-outlined">import_contacts</span>
             <span class="label">My Shelf</span>
           </router-link>
         </li>
 
         <li class="menu-item">
           <router-link to="/next-read">
-            <span class="material-symbols-outlined">auto_awesome</span>
-            <span class="label">Pick for Me</span>
+            <span class="icon material-symbols-outlined">explore</span>
+            <span class="label">My Next Read</span>
           </router-link>
         </li>
       </ul>
